c395125bbc5f65fc3e91d940681f19b4312c02ce
michal7272 <michal7272@seznam.cz>
2023-05-22 00:27:39 +0200

    add menu icon animation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 // Responsive navigation
-const menuIcon = document.querySelector('.nav-menu-icon');
+const menuBtn = document.querySelector('.nav-menu-btn');
 const nav = document.querySelector('.nav-main');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Collapse or expand navigation when the menu icon is clicked
-menuIcon.addEventListener('click', () => {
+menuBtn.addEventListener('click', () => {
     nav.classList.toggle('show');
+    menuBtn.classList.toggle('close');
 });
 
 // Collapse navigation when a link is clicked
 navItems.forEach(e => {
     e.addEventListener('click', () => {
         nav.classList.toggle('show');
+        menuBtn.classList.toggle('close');
     });
 });
