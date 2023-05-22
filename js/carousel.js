f915cc0ef55698fc01972f9047af5e712dfef40d
michal7272 <michal7272@seznam.cz>
2023-05-22 23:19:34 +0200

    add carousel

---
+++ js/carousel.js
@@ -0,0 +1,47 @@
+// Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      // nextEl: '.swiper-button-next',
+      // prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+});
