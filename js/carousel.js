baa8877abb4bce9b8a225ce80ad23cbee5a04f25
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-11 16:13:59 +0100

    Change indent to 2 spaces

--- js/carousel.js
+++ js/carousel.js
@@ -1,45 +1,45 @@
 // Swiper
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 
-    // Default parameters
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 20,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
     slidesPerView: 1,
-    spaceBetween: 20,
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 320px
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      992: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      1600: {
-        slidesPerView: 4,
-        spaceBetween: 20
-      }
+    spaceBetween: 20
+    },
+    768: {
+    slidesPerView: 1,
+    spaceBetween: 20
+    },
+    992: {
+    slidesPerView: 2,
+    spaceBetween: 20
+    },
+    1200: {
+    slidesPerView: 3,
+    spaceBetween: 20
+    },
+    1600: {
+    slidesPerView: 4,
+    spaceBetween: 20
     }
+  }
 });
--- js/navigation.js
+++ js/navigation.js
@@ -5,14 +5,14 @@ const navItems = document.querySelectorAll('.nav-item');
 
 // Collapse or expand navigation when the menu icon is clicked
 menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('show');
-    menuBtn.classList.toggle('close');
+  nav.classList.toggle('show');
+  menuBtn.classList.toggle('close');
 });
 
 // Collapse navigation when a link is clicked
 navItems.forEach(e => {
-    e.addEventListener('click', () => {
-        nav.classList.toggle('show');
-        menuBtn.classList.toggle('close');
-    });
+  e.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    menuBtn.classList.toggle('close');
+  });
 });
